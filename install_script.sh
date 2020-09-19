069f96a6f8db8aae733dad4849cada7ca7bfa403
Lukas Buchli <13186358+lbuchli@users.noreply.github.com>
2020-09-19 21:05:08 +0200

    Make faker script executable

--- install_script.sh
+++ install_script.sh
@@ -170,6 +170,7 @@ arch-chroot /mnt mkinitcpio -P
 # Use the faker script for grub-probe
 mv /mnt/bin/grub-probe /mnt/bin/grub-probe.orig
 cp grub-probe /mnt/bin
+chmod +x /mnt/bin/grub-probe
 
 # GRUB sanity check
 arch-chroot /mnt grub-probe /boot
