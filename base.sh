c32726ff47c9fc1ed503220786fd51f6f0fbcb84
Lukas Buchli <lukas@buchli-bigger.ch>
2020-09-22 16:26:32 +0200

    Create EFI folder

--- base.sh
+++ base.sh
@@ -156,6 +156,7 @@ mount --rbind /dev /mnt/dev
 
 # Install grub (for EFI)
 print -P "${CYAN}Installing GRUB...${NC}"
+mkdir /mnt/boot/efi
 chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --recheck
 
 # Configure grub
